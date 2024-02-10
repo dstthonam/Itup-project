132465be8f34850642e01122ce20c486ccd68500
inseoung <thonam123@naver.com>
2024-02-10 16:17:36 +0900

    MBTI website
    MBTI 성향 조합 테스트 웹 사이트 완성

--- javascript/practice2.js
+++ javascript/practice2.js
@@ -75,4 +75,3 @@ for (let num = 0; num < 10; num = num + 1)
 }
 
 //
-
---
+++ javascript/practice3.js
@@ -0,0 +1,53 @@
+
+let students = ["peter", "john", "king"]
+for (let index in students){
+  console.log(students[index]);
+}
+
+// 
+
+let dt1 = ["fun", "coding", "Dave", "Lee", "업무", "자동화"]
+for (let index in dt1){
+  console.log(dt1[index]);
+}
+
+// 
+
+let datas1 = [2, 4, 6, 8, 10]
+for (let index in datas1){
+  console.log((datas1[index] * 2))
+}
+
+// 
+
+let grades = {"test1": 10, "test2": 6, "test3": 80};
+let grades = {};
+
+// 
+
+let dtb1 = {"name":"누구나 쓱 읽고 싹 이해하는 IT 핵심 기술", "category":"IT", "author":"thonam"}
+for (let key in dtb1) {
+  console.log(dtb1[key]);
+}
+
+// 
+
+let roadmap = {"dataroadmap":["데이터 수집", "데이터 저장", "데이터 분석", "데이터 예측(머신러닝)", "데이터 예측(딥러닝)"], "fullstackroadmap":["HTML, CSS, JS", "python", "flask, 백엔드", "docker, linux"]}
+for (let key in roadmap.fullstackroadmap) {
+  console.log(roadmap.fullstackroadmap[key]);
+}
+
+// 
+
+
+
+// 
+
+
+
+// 
+
+
+
+// 
+
